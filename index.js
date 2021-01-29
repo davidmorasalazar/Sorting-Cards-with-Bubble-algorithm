bea24590a817ee88e7b496bba7cadb26fc95bc1f
davidmorasalazar <davidmsalazar99@gmail.com>
2021-01-29 23:05:11 +0000

    my commit 2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import "index.scss";
+let symbol = document.getElementById("span");  // llama a la funcion div del html
+let number = document.getElementById("span1") ; // llama a la funcion div del html
+let symbol1 = document.getElementById("span2");  // llama a la funcion div del html
+let number1 = document.getElementById("span3") ; // llama a la funcion div del html
+let symbol2 = document.getElementById("span4");  // llama a la funcion div del html
+let number2 = document.getElementById("span5") ; // llama a la funcion div del html
+let symbol3 = document.getElementById("span6");  // llama a la funcion div del html
+let number3 = document.getElementById("span7") ; // llama a la funcion div del html
+
 var sortMeglobal; //define esta variable global para luego usarla en la otra funcion.
- 
  function processFormData() {
     var arrayLength = document.getElementById("fnumber").value;  // llama a la funcion div del html
     //  console.log(arrayLength)
@@ -15,9 +22,19 @@ var sortMeglobal; //define esta variable global para luego usarla en la otra fun
      for(; i < arrayLength; i++) {
          logText += sortMe[i] + ", ";
      } 
+      let symbolarray = ["&diams;","&spades;","&hearts;","&clubs;"] ;
+      var randomsymbol = Math.floor(Math.random()*symbolarray.length);
+      symbol.innerHTML = symbolarray[randomsymbol]; 
+      symbol1.innerHTML = symbolarray[randomsymbol]; 
+      symbol2.innerHTML = symbolarray[randomsymbol]; 
+      symbol3.innerHTML = symbolarray[randomsymbol]; 
     //  console.log("Starting Values: \t" + logText);
      console.log(sortMe) 
      sortMeglobal = sortMe;
+      number.innerHTML = sortMeglobal[0];
+      number1.innerHTML = sortMeglobal[1];
+      number2.innerHTML = sortMeglobal[2];
+      number3.innerHTML = sortMeglobal[3];
     // processFormData1(sortMe)
 // }
 //   aleatorios()
