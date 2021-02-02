dbed9ac1f8a0aa754e9b6ea3d041a8e80fd03df8
davidmorasalazar <davidmsalazar99@gmail.com>
2021-02-02 17:01:48 +0000

    my commit 6

--- index3.js
+++ index3.js
@@ -1,6 +1,9 @@
 var sortMeglobal; //define esta variable global para luego usarla en la otra funcion.
 var arrayLengthglobal;
+var symbolglobal;
 function processFormData() {
+    container.innerHTML = "";
+    // container1.innerHTML = "";
     var arrayLength = document.getElementById("fnumber").value;  
      var sortMe = new Array();
      var i = 0;
@@ -26,9 +29,11 @@ function processFormData() {
      console.log(sortMe) 
       sortMeglobal = sortMe;
       arrayLengthglobal = arrayLength;
+    //   card.classList.remove("card")
 }
 
 function processFormData1() {
+    container1.innerHTML = "";
     let sortMe1 = sortMeglobal;
     let wall = sortMe1.length - 1; //we start the wall at the end of the array
     while (wall > 0){
@@ -43,12 +48,26 @@ function processFormData1() {
           index++;
         }
         wall--; //decrease the wall for optimization
-    }
+    };    
+    while (wall > 0){
+        let index = 0;
+        while (index < wall) {
+          //compare the adjacent positions, if the right one is bigger, we have to swap
+          if (sortMe1[index] > sortMe1[index + 1]) { 
+            let aux = symbol[index]; 
+            symbol[index] = symbol[index + 1];
+            symbol[index + 1] = aux;
+          }
+          index++;
+        }
+        wall--; //decrease the wall for optimization
+    };
+    symbolglobal = symbol;
     for(i=0; i < sortMe1.length; i++) {
         let sort = document.createElement("div");
         sort.classList.add("sort");
         let p = document.createElement("span");
-        p.innerHTML = symbol[i];
+        p.innerHTML = symbolglobal[i];
         let n = document.createElement("span");
         n.innerHTML = sortMe1[i];
         sort.appendChild(p);
@@ -56,4 +75,5 @@ function processFormData1() {
         container1.appendChild(sort);
         console.log(sortMe1);
 }
-;}
+    };
+
